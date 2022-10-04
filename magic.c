5f5539c8ff770d2d86f2021aa3cdcfe4488360a9
Afaiwjs6272 <113013173+Afaiwjs6272@users.noreply.github.com>
2022-10-04 16:42:44 +0300

    Add files via upload

---
+++ magic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+    scanf("%x", &a);
+    scanf ("%o", &b);
+    printf("%u\n", a);
+    printf("%o\n", a);
+    printf("%o\n", a>>1);
+    printf("%o\n", ~a);
+    printf ("%o\n", (a|b));
+    return 0;
+}
