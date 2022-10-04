821ed9602c0b96a264ea378749a7321dd76452dd
Afaiwjs6272 <113013173+Afaiwjs6272@users.noreply.github.com>
2022-10-04 16:43:12 +0300

    Add files via upload

--- magic.c
+++ magic.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-int main() {
-    int a;
-    int b;
-    scanf("%x", &a);
-    scanf ("%o", &b);
-    printf("%u\n", a);
-    printf("%o\n", a);
-    printf("%o\n", a>>1);
-    printf("%o\n", ~a);
-    printf ("%o\n", (a|b));
-    return 0;
-}
+int main () {
+    double mak[7] = {10, 20, 30, 40,50,60,70};
+    double *a = mak;
+    int mas[2][2]={1,1,1,4};
+    int mas2[2][2]={1,2,4,1};
+    int mas3[2][2];
+    for (int i = 0; i < 7; i++) {
+        printf("%lf\n", *(a + i));
+    }
+        mas3[0][0]=mas[0][0]*mas2[0][0]+mas[0][1]*mas2[1][0];
+        mas3[0][1]=mas[0][0]*mas2[0][1]+mas[0][1]*mas2[1][1];
+        mas3[1][0]=mas[1][0]*mas2[0][0]+mas[1][1]*mas2[1][0];
+        mas3[1][1]=mas[1][0]*mas2[0][1]+mas[1][1]*mas2[1][1];
+        printf("\n");
+        printf("%d",mas3[0][0]);
+        printf(" ");
+        printf("%d\n",mas3[0][1]);
+        printf("%d",mas3[1][0]);
+        printf(" ");
+        printf("%d",mas3[1][1]);
+        return 0;
+    }
