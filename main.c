c4055fd30187314f1103d3bf4c51335ad0b1ee68
Afaiwjs6272 <113013173+Afaiwjs6272@users.noreply.github.com>
2022-10-05 10:26:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main () {
-    double mak[7] = {10, 20, 30, 40,50,60,70};
+    int a[]={10, 20, 30, 40, 50, 60, 70};
+    printf("%d %d %d %d %d %d %d\n",a[0],a[1],a[2],a[3],a[4],a[5],a[6]);
     int mas[2][2]={1,1,1,4};
     int mas2[2][2]={1,2,4,1};
     int mas3[2][2];
-    for (int i = 0; i < 7; i++) {
-        printf("%lf\n", *(mak + i));
-    }
+         printf("%d %d %d %d %d %d %d\n",a[0],a[1],a[2],a[3],a[4],a[5],a[6]);
     mas3[0][0]=mas[0][0]*mas2[0][0]+mas[0][1]*mas2[1][0];
     mas3[0][1]=mas[0][0]*mas2[0][1]+mas[0][1]*mas2[1][1];
     mas3[1][0]=mas[1][0]*mas2[0][0]+mas[1][1]*mas2[1][0];
